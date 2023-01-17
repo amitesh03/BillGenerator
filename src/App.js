5142c50381caecd3eab3f6e6dd61d87ec6e47444
Amitesh Patnaik <49634729+amitesh03@users.noreply.github.com>
2023-01-17 15:42:46 +0530

    added bill generator home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <>
-    Bill Generator CRA boilerplate setup
+    <div className='img'>
+    <img src="../public/bg-sidebar-desktop.svg" alt="image" />
+    </div>
+    <div className='info'>
+    <h1 className='heading'>Your Information</h1>
+    <form className='form' action="">
+    <label htmlFor="name">Name</label>
+    <input type="text" placeholder='Enter your name' />
+    <label htmlFor="email">Email</label>
+    <input type="email" placeholder='Enter your email'/>
+    <label htmlFor="phone">Phone</label>
+    <input type="tel" placeholder='Enter your Phone Number'/>
+    </form>
+    </div>
     </> 
   );
 }
